31197c07a8c3672470896976e66fee77655c998b
Amir Shahzad <Amir.Shahzad@wisemani.com>
2025-03-03 10:59:03 +0500

    Fixation

--- pages/patientportal/dashboard/index.tsx
+++ pages/patientportal/dashboard/index.tsx
@@ -2,15 +2,25 @@ import GetToken from "@/components/GetToken/GetToken";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "@/store/index";
 import { GetPatientByEmail } from "@/slices/patientprofileslice";
+import { useRouter } from 'next/router';
 
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
   const types = `john.doe@gmail.com`;
   const { PatientByEmailData } = useSelector((state) => state.patientprofileslice);
 
   useEffect(() => {
     dispatch(GetPatientByEmail(types));
   }, [dispatch]);
+
+
+  useEffect(() => {
+    // Redirect to /patientportal/profile when the component mounts
+    router.push('/patientportal/profile');
+  }, [router]);
+
+
 
   const patient = PatientByEmailData?.[0];
   localStorage.setItem('patientEmail', types);
